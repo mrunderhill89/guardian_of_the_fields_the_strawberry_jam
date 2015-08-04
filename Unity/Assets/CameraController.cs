49958443f0dda96b762f7a94f2942eefd2bc0a5a
mrunderhill89 <mrunderhill89@gmail.com>
2015-08-04 15:59:47 -0700

    Camera now changes position when states change.

---
+++ Unity/Assets/CameraController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	public Transform target;
+	public Transform track_to;
+	public Vector3 rotation_limit;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
--- Unity/Assets/GameStateManager.cs
+++ Unity/Assets/GameStateManager.cs
@@ -5,9 +5,20 @@ using System;
 public class GameStateManager : MonoBehaviour {
 	public HFSM_State root;
 	public InputController input;
+	public Transform camera_transform = null;
+	public Transform c_look_forward = null,c_look_left = null,c_look_right = null;
+	public Transform c_pick_left = null,c_pick_right = null,c_pack = null;
 	Action lazy_log(string message){
 		return () => {
 			Debug.Log (message);
+		};
+	}
+	Action set_camera(Transform t){
+		return () => {
+			if (t != null){
+				camera_transform.position = t.position;
+				camera_transform.rotation = t.rotation;
+			}
 		};
 	}
 	void Start () {
@@ -17,11 +28,11 @@ public class GameStateManager : MonoBehaviour {
 		};
 		//Look
 		HFSM_State look_forward = new HFSM_State ()
-			.on_entry(lazy_log("Look Forward"));
+			.on_entry(set_camera(c_look_forward));
 		HFSM_State look_left = new HFSM_State ()
-			.on_entry(lazy_log("Look Left"));
+			.on_entry(set_camera(c_look_left));
 		HFSM_State look_right = new HFSM_State ()
-			.on_entry(lazy_log("Look Right"));
+			.on_entry(set_camera(c_look_right));
 		HFSM_State look = new HFSM_State ()
 			.add_child(look_forward)
 			.add_child(look_left)
@@ -31,9 +42,9 @@ public class GameStateManager : MonoBehaviour {
 
 		//Pick
 		HFSM_State pick_left = new HFSM_State ()
-			.on_entry(lazy_log("Pick Left"));
+			.on_entry(set_camera(c_pick_left));
 		HFSM_State pick_right = new HFSM_State ()
-			.on_entry(lazy_log("Pick Right"));
+			.on_entry(set_camera(c_pick_right));
 		HFSM_State pick = new HFSM_State ()
 			.add_child(pick_left)
 			.add_child(pick_right)
@@ -41,7 +52,7 @@ public class GameStateManager : MonoBehaviour {
 
 		//Pack
 		HFSM_State pack = new HFSM_State ()
-			.on_entry(lazy_log("Packing"));
+			.on_entry(set_camera(c_pack));;
 
 		//Transitions
 		//Look Forward -> Look Left, Look Right, Pack
