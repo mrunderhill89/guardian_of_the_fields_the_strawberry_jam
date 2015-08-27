d37aff3e9537d0846731d44afca51b0d169593dc
Kevin <mrunderhill89@gmail.com>
2015-08-27 14:59:31 -0700

    Double-checking to make sure I didn't leave anything out from yesterday.

---
+++ Unity/Assets/Scripts/Grounded.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Rigidbody))]
+public class Grounded : MonoBehaviour {
+	Rigidbody body;
+	public float floor_height = 0.0f;
+	// Use this for initialization
+	void Start () {
+		body = gameObject.GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.y < floor_height) {
+			transform.position = new Vector3(transform.position.x, floor_height, transform.position.z);
+			body.velocity = new Vector3(body.velocity.x, 0.0f, body.velocity.z);
+		}
+	}
+}
