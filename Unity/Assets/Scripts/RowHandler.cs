7ba655034e4c537459f5eeb2122e427ee710bc50
Kevin <mrunderhill89@gmail.com>
2015-10-06 15:50:19 -0700

    Added back empty rows using the new row handling system. Also split up row handling and strawberry row handling so it's not so tightly coupled. There's still a long load-up time, but once that's done the game runs surprisingly fast, even on my laptop.

---
+++ Unity/Assets/Scripts/RowHandler.cs
@@ -0,0 +1,103 @@
+﻿using Vexe.Runtime.Types;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using UniRx;
+public class RowHandler : BetterBehaviour{
+	public float create_distance = 20.0f;
+	public float remove_distance = -10.0f;
+	public float cell_distance = 1.0f;
+
+	public string prefab = "GroundCell";
+	protected List<GameObject> cells;
+
+	public List<Action<GameObject>> create_events;
+	public RowHandler on_create(Action<GameObject> act){
+		create_events.Add (act);
+		return this;
+	}
+
+	public List<Action<GameObject>> destroy_events;
+	public RowHandler on_destroy(Action<GameObject> act){
+		destroy_events.Add (act);
+		return this;
+	}
+
+	[DontSerialize]
+	new public Camera camera;
+
+	void Awake(){
+		cells = new List<GameObject>();
+		create_events = new List<Action<GameObject>>();
+		destroy_events = new List<Action<GameObject>>();
+	}
+	void Start(){
+		if (camera == null) camera = Camera.main;
+	}
+	void Update(){
+		generate_cells_to_distance(get_create_distance());
+		trim_cells_to_distance(get_remove_distance());
+	}
+	
+	float get_furthest_cell_distance(){
+		if (cells.Count > 0){
+			return cells.Last().transform.position.z;
+		}
+		return get_remove_distance();
+	}
+	float get_furthest_back_distance(){
+		if (cells.Count > 0){
+			return cells.First().transform.position.z;
+		}
+		return get_remove_distance();
+	}
+	float get_create_distance(){
+		return camera.transform.position.z + create_distance;
+	}
+	float get_remove_distance(){
+		return camera.transform.position.z + remove_distance;
+	}
+
+	public int Count{
+		get{return cells.Count;}
+	}
+
+	public GameObject cell(int index){
+		return cells [index];
+	}
+	GameObject generate_cell(){
+		float z = get_furthest_cell_distance()+cell_distance;
+		GameObject cell = GameObject.Instantiate(Resources.Load(prefab)) as GameObject;
+		cell.transform.localPosition = new Vector3(transform.localPosition.x,transform.localPosition.y, z);
+		cell.transform.SetParent(transform,true);
+		cells.Add(cell);
+		foreach(Action<GameObject> act in create_events){
+			act(cell);
+		}
+		return cell;
+	}
+
+	void pop_cell(){
+		if (cells.Count > 0){
+			GameObject cell = cells[0];
+			foreach(Action<GameObject> act in destroy_events){
+				act(cell);
+			}
+			cells.RemoveAt(0);
+			Destroy(cell);
+		}
+	}
+
+	void generate_cells_to_distance(float d){
+		while (get_furthest_cell_distance() < d){
+			generate_cell();
+		}
+	}
+	void trim_cells_to_distance(float d){
+		while (get_furthest_back_distance() < d){
+			pop_cell();
+		}
+	}
+}
--- Unity/Assets/Scripts/StrawberryRowState.cs
+++ Unity/Assets/Scripts/StrawberryRowState.cs
@@ -4,6 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System;
+
 public class StrawberryRowState : BetterBehaviour{
 	[DontSerialize]
 	public static List<StrawberryRowState> rows;
@@ -18,87 +19,37 @@ public class StrawberryRowState : BetterBehaviour{
 		return null;
 	}
 	public State state;
-	public float create_distance = 20.0f;
-	public float remove_distance = -10.0f;
-	public float cell_distance = 1.0f;
+	public RowHandler row;
 	public int num_receiving = 1;
-	protected List<GameObject> cells;
 	[DontSerialize]
 	public GameStateManager player;
-	[DontSerialize]
-	new public Camera camera;
 	[DontSerialize]
 	public StrawberryStateMachine berry_state;
 	void Awake(){
 		rows.Add(this);
 		state = NamedBehavior.GetOrCreateComponentByName<State>(gameObject, "row")
 			.initial(distribute);
-		cells = new List<GameObject>();
 	}
 	void Start(){
+		if (row == null) row = GetComponent<RowHandler> ();
 		if (berry_state == null) berry_state = SingletonBehavior.get_instance<StrawberryStateMachine>();
 		if (player == null) player = SingletonBehavior.get_instance<GameStateManager>();
-		if (camera == null) camera = Camera.main;
 		state.parent(berry_state.fsm.state("field"));
-	}
-	void Update(){
-		generate_cells_to_distance(get_create_distance());
-		trim_cells_to_distance(get_remove_distance());
-	}
-
-
-	float get_furthest_cell_distance(){
-		if (cells.Count > 0){
-			return cells.Last().transform.position.z;
-		}
-		return get_remove_distance();
-	}
-	float get_furthest_back_distance(){
-		if (cells.Count > 0){
-			return cells.First().transform.position.z;
-		}
-		return get_remove_distance();
-	}
-	float get_create_distance(){
-		return camera.transform.position.z + create_distance;
-	}
-	float get_remove_distance(){
-		return camera.transform.position.z + remove_distance;
+		row.on_create((GameObject cell) => {
+			StrawberryGenerator generator = cell.GetComponent<StrawberryGenerator>();
+			generator.state.parent(state);
+		}).on_destroy((GameObject cell) => {
+			StrawberryGenerator generator = cell.GetComponent<StrawberryGenerator>();
+			generator.PreDestroy();
+		});
 	}
 
-	GameObject generate_cell(){
-		float z = get_furthest_cell_distance()+cell_distance;
-		GameObject cell = GameObject.Instantiate(Resources.Load ("GroundCell")) as GameObject;
-		cell.transform.localPosition = new Vector3(transform.localPosition.x,transform.localPosition.y, z);
-		cell.transform.SetParent(transform,true);
-		cell.GetComponent<StrawberryGenerator>().state.parent(state);
-		cells.Add(cell);
-		return cell;
-	}
-	void pop_cell(){
-		if (cells.Count > 0){
-			GameObject back_cell = cells[0];
-			cells.RemoveAt(0);
-			back_cell.GetComponent<StrawberryGenerator>().PreDestroy();
-			Destroy(back_cell);
-		}
-	}
-	void generate_cells_to_distance(float d){
-		while (get_furthest_cell_distance() < d){
-			generate_cell();
-		}
-	}
-	void trim_cells_to_distance(float d){
-		while (get_furthest_back_distance() < d){
-			pop_cell();
-		}
-	}
 	State distribute(Automata a){
 		//If the game is just starting, we can place into any cell we want.
 		int front_index = player.is_loading()?
-			0:Math.Max(cells.Count - num_receiving,0);
-		if (cells.Count > 0){
-			GameObject cell = cells[RandomUtils.random_int(front_index,cells.Count)];
+			0:Math.Max(row.Count - num_receiving,0);
+		if (row.Count > 0){
+			GameObject cell = row.cell(RandomUtils.random_int(front_index,row.Count));
 			return cell.GetComponent<StrawberryGenerator>().state;
 		}
 		return null;
