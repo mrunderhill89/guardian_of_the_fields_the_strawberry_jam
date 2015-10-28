c01d010c7ca22301ab17cc25e4a121353f76c00e
mrunderhill89 <mrunderhill89@gmail.com>
2015-10-28 16:54:31 -0700

    Main menu connects to the game scene
    Also experimented with different ways of setting up the sky. I think we
    should stick with cylinders but for some reason I'm getting a framerate
    drop with the new one I made. I'll see if it's just a problem with my
    machine (remember that the laptops at the NEST are probably better than
    anything I'm running, so as long as it's basically playable on mine, it
    should be okay there).

---
+++ Unity/Assets/QuitGame.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuitGame : MonoBehaviour {
+	public void trigger(){
+		Application.Quit();
+	}
+}
---
+++ Unity/Assets/StartGame.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGame : MonoBehaviour {
+	public void trigger(){
+		Application.LoadLevel("main");
+	}
+}
