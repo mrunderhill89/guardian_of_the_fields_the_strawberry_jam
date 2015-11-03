0840378b70a92f7dff2e4c2a7a3e4f3320a28b4e
mrunderhill89 <mrunderhill89@gmail.com>
2015-11-02 21:51:45 -0800

    Added text labels to the Pace and Compass GUI icons.

---
+++ Unity/Assets/Scripts/GameStateLabel.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using Vexe.Runtime.Types;
+
+public class GameNameLabel : BetterBehaviour {
+	public GameStateManager game;
+	public Text text;
+	public Dictionary<string,string> state_names = new Dictionary<string,string>();
+}
---
+++ Unity/Assets/Scripts/PaceNameLabel.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using Vexe.Runtime.Types;
+[ExecuteInEditMode]
+public class PaceNameLabel : BetterBehaviour {
+	public PaceManager pace;
+	public Slider slider;
+	public Text text;
+	public SortedList<float,string> pace_names = new SortedList<float,string>();
+	void Start(){
+		if (slider != null){
+			slider.onValueChanged.AddListener(this.update);
+			this.update(slider.value);
+		}
+	}
+	
+	void update(float value){
+		if (pace != null && text != null){
+			foreach(KeyValuePair<float,string> pair in pace_names){
+				if (value < pair.Key) break;
+				text.text = pair.Value;
+			}
+		}
+	}
+}
