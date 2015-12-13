82ae264c8dd13f1590d7562a3576c3e9fc90bf75
mrunderhill89 <mrunderhill89@gmail.com>
2015-12-12 17:35:34 -0800

    Footstep tweaks, UI tweaks, and a bit more script reshuffling.
    Footsteps are now a bit slower, and the color sliders under settings now
    have their own gradient and handle instead of the Unity defaults. I also
    added a clip of ambient sound, but for some reason Unity won't import it
    on Linux, so I'll try again on my laptop to see if that works.

---
+++ Unity/Assets/Scripts/UserInterface/StrawberryColorIcon.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using Vexe.Runtime.Types;
+public class StrawberryColorIcon : BetterBehaviour {
+
+	public Slider slider;
+	public Image icon;
+	[Show]
+	public float value{
+		get{return slider.value / StrawberryColor.max_quality;}
+	}
+	[Show]
+	public Color color{
+		get{return StrawberryColor.color_gradient.Evaluate(value);}
+	}
+	// Update is called once per frame
+	void Update () {
+		icon.color = color;
+	}
+}
---
+++ Unity/Assets/Scripts/UserInterface/TutorialHandler.cs
@@ -0,0 +1,22 @@
+﻿using Vexe.Runtime.Types;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using UniRx;
+public class TutorialHandler : BetterBehaviour {
+	[Serializable]
+	public class TutorialMessage{
+		public void display(){
+			GameMessages.Log("Tutorial Message!");
+		}
+	}
+	public static Dictionary<string,TutorialMessage> messages = new Dictionary<string,TutorialMessage>();
+	
+	public static void Log(string key){
+		if (messages.ContainsKey(key)){
+			messages[key].display();
+		}
+	}
+}
