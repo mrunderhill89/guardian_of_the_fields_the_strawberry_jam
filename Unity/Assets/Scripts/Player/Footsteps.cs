04e7040a00ba998702572c705c0cd2c5a8d1b205
mrunderhill89 <mrunderhill89@gmail.com>
2015-12-17 17:13:45 -0800

    Minor tweaks
    Ground tiles no longer cast their own shadows for performance reasons
    (needs testing). Reworked the footsteps component a bit so that the
    rhythm can be tweaked independently of the player's speed. Added two
    buttons to the loading screen. One lets the player return to the menu
    before the game loads in case they clicked "start" by accident. The
    other will eventually bring up a copy of the controls menu so the player
    can review if they didn't see it earlier.

--- Unity/Assets/Scripts/Player/Footsteps.cs
+++ Unity/Assets/Scripts/Player/Footsteps.cs
@@ -13,8 +13,12 @@ public class Footsteps : BetterBehaviour {
 	public AudioSource right_foot_source;
 
 	public float speed_adjust = 1.0f;
+	public float volume_adjust = 1.0f;
+	protected float record_z = 0.0f;
+	public float max_delta_z = 1.0f;
 	public float foot_position = 0.0f;
 
+	public AnimationCurve pace_curve = new AnimationCurve();
 	public AnimationCurve volume_curve = new AnimationCurve();
 
 	public AudioClip left_foot_sound(){
@@ -26,14 +30,18 @@ public class Footsteps : BetterBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		float new_foot_position = Mathf.Sin (transform.position.z * speed_adjust);
-		float volume = volume_curve.Evaluate(Mathf.Abs (foot_position-new_foot_position)/2.0f);
-		if (new_foot_position > 0.0f && foot_position < 0.0f) {
+		float delta_z = transform.position.z - record_z;
+		float new_foot_position = foot_position + (pace_curve.Evaluate(delta_z / max_delta_z) * speed_adjust);
+		float s_old = Mathf.Sin(foot_position);
+		float s_new = Mathf.Sin(new_foot_position);
+		float volume = volume_adjust * volume_curve.Evaluate(Mathf.Abs(s_new - s_old)/2.0f);
+		if (s_new > 0.0f && s_old < 0.0f) {
 			left_foot_source.PlayOneShot(left_foot_sound(), volume);
 		}
-		if (new_foot_position < 0.0f && foot_position > 0.0f) {
+		if (s_new < 0.0f && s_old > 0.0f) {
 			right_foot_source.PlayOneShot(right_foot_sound(), volume);
 		}
 		foot_position = new_foot_position;
+		record_z = transform.position.z;
 	}
 }
---
+++ Unity/Assets/Scripts/UserInterface/MainMenuReturn.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuReturn : MonoBehaviour {
+	public void trigger(){
+		Application.LoadLevel("title_screen");
+	}
+}
