efc33c4cfc377041a7e88d64f81247dbfedcd96a
mrunderhill89 <mrunderhill89@gmail.com>
2015-12-26 20:52:46 -0800

    Added a silhouette under each strawberry to help with depth perception.
    Basically it's a second billboard that raycasts towards the ground and
    places itself where the strawberry will fall once the player lets go.

---
+++ Unity/Assets/StrawberryFootprint.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StrawberryFootprint : MonoBehaviour {
+
+	public DragHandle drag;
+	public ObjectVisibility vis;
+	public Transform target;
+	public float max_distance = 2.0f;
+	void Update () {
+		if (drag.is_dragging()){
+			RaycastHit hit;
+			if (Physics.Raycast(target.position, Vector3.down, out hit, max_distance, drag.layer_mask, QueryTriggerInteraction.Ignore)) {
+				transform.position = hit.point;
+				vis.visible = true;
+				return;
+			}
+		}
+		vis.visible = false;
+	}
+}
