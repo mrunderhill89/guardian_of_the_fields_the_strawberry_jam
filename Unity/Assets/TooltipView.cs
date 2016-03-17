20c39ffdd78bcfe558ac076ed80e7d832cf33000
mrunderhill89 <mrunderhill89@gmail.com>
2016-03-17 15:37:49 -0700

    Constrained the tooltip to the bounds of the screen with some padding, so that the info box doesn't slide off the screen when the mouse is near the edge.

--- Unity/Assets/TooltipView.cs
+++ Unity/Assets/TooltipView.cs
@@ -9,7 +9,9 @@ using UniRx;
 public class TooltipView : BetterBehaviour {
 	public Text text;
 	public ObjectVisibility vis;
-	
+	public float x_padding = 160;
+	public float y_padding = 30;
+
 	void Start(){
 		vis = ObjectVisibility.GetVisibility(gameObject, vis);
 		hide();
@@ -17,7 +19,12 @@ public class TooltipView : BetterBehaviour {
 	
 	void Update(){
 		if (vis.visible){
-			transform.position = Input.mousePosition;
+			transform.position = new Vector3(
+				Mathf.Clamp(Input.mousePosition.x, x_padding, Screen.width - x_padding),
+				Mathf.Clamp(Input.mousePosition.y, y_padding, Screen.height - y_padding),
+				transform.position.z
+			);
+
 		}
 	}
 	
