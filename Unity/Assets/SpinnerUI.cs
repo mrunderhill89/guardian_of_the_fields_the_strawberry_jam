57e6f9e6982498b5e4c1bc30a7c528eb2a9a772f
mrunderhill89 <mrunderhill89@gmail.com>
2016-04-19 17:11:45 -0700

    Polished the gameplay settings screen.
    Removed Win Condition settings for the time being until I can get a good
    interface for the new ScoreTarget class, but they will be back soon. Now
    each section has labels so it looks cleaner, too.

---
+++ Unity/Assets/SpinnerUI.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using Vexe.Runtime.Types;
+using UniRx;
+
+public class SpinnerUI : BetterBehaviour {
+	public InputField input;
+	public Button plus;
+	public Button minus;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
